0920325966628e641ada57d3f0e5c6447b57a8c6
Ferdi ÜNAL <ferdiunal@outlook.com>
2024-09-16 21:18:44 +0300

    Refactor tailwind.config.js to include dark mode support

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ import { generateTailwindColors } from '../../vendor/laravel/nova/generators'
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./resources/**/*.{js,vue}'],
-  darkMode: 'class',
+  darkMode: ['class', '[class="dark"]'],
   prefix: 'ln-',
   theme: {
     colors: generateTailwindColors(),
